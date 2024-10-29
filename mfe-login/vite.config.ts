bb5706e64f74b13a3b87ecc17b5f001f4544da35
William <williamalonso@outlook.com>
2024-10-29 19:28:46 -0300

    :zap: configuring

--- mfe-login/vite.config.ts
+++ mfe-login/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { federation } from '@module-federation/vite'; // Importa o plugin de federação de módulos
+// import { federation } from '@module-federation/vite'; // Importa o plugin de federação de módulos
+import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
   plugins: [
