f414cb6d0dc9b34148aef6fd212ffc847d5f10e5
William <williamalonso@outlook.com>
2024-10-30 13:08:36 -0300

    :zap: creating pagination

--- host/src/routes/AppRoutes.jsx
+++ host/src/routes/AppRoutes.jsx
@@ -1,20 +1,18 @@
 // src/routes/AppRoutes.jsx
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-// Importando componentes locais
+// Importando paginas locais
 import Home from '../pages/Home';
 import PartnersPage from '../pages/PartnersPage'; // Nova página de parceiros
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Carregando...</div>}>
         <Routes>
           <Route path="/" element={<Home />} /> {/* Rota para Home */}
           <Route path="/partners" element={<PartnersPage />} /> {/* Rota para a página de parceiros */}
         </Routes>
-      </Suspense>
     </BrowserRouter>
   );
 };
