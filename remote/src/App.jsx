fed993c3cd0a10a794e0dc245f4cf513764812ae
William <williamalonso@outlook.com>
2024-10-30 12:41:55 -0300

    :zap: creating pagination

--- remote/src/App.jsx
+++ remote/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import './App.css';
 import Button from './components/button/Button';
 import PartnersList from './components/partners/PartnersList';
 
@@ -7,16 +8,23 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <PartnersList />
-      <h1>Remote Application</h1>
-      <Button />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <Router>
+      <div className="App">
+        <h1>Remote Application</h1>
+        <Button />
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+
+        {/* Definindo as rotas */}
+        <Routes>
+          <Route path="/" element={<PartnersList />} /> {/* Rota principal */}
+          {/* Adicione outras rotas conforme necessário */}
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
