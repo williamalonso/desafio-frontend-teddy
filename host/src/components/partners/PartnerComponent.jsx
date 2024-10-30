3949318a6898decd08709b53aa9c255cc5dc6216
William <williamalonso@outlook.com>
2024-10-30 17:54:54 -0300

    :zap: pagination ok

--- host/src/components/partners/PartnerComponent.jsx
+++ host/src/components/partners/PartnerComponent.jsx
@@ -1,15 +1,34 @@
-import React, { Suspense } from 'react';
-
-import PartnersList from "remoteApp/PartnersList";
+// src/PartnerComponent.jsx
+import React, { Suspense, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom'; 
+import PartnersList from 'remoteApp/PartnersList';
 
 const PartnerComponent = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const initialPage = Number(searchParams.get('index')) || 1; // Lê a página da URL
+  const [currentPage, setCurrentPage] = useState(initialPage);
+
+  // Sincroniza a URL sempre que a página atual muda
+  useEffect(() => {
+    setSearchParams({ index: currentPage });
+    console.log('Current page:', currentPage);
+  }, [currentPage, setSearchParams]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page); // Atualiza o estado com a nova página
+  };
+
   return (
     <div>
       <Suspense fallback={<div>Carregando lista de parceiros...</div>}>
-          <PartnersList />
+        <PartnersList 
+          currentPage={currentPage} 
+          onPageChange={handlePageChange} 
+        />
       </Suspense>
     </div>
   );
-}
- 
+};
+
 export default PartnerComponent;
--- remote/src/components/partners/PartnersList.jsx
+++ remote/src/components/partners/PartnersList.jsx
@@ -4,20 +4,21 @@ import axios from 'axios';
 import AddPartnerModal from '../addModal/AddPartnerModal';
 import PartnerModal from '../editModal/PartnerModal';
 
-const PartnersList = () => {
-
+const PartnersList = ({ currentPage, onPageChange }) => {
+  
   const [partners, setPartners] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPartner, setCurrentPartner] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(5); // Número de itens por página
 
   useEffect(() => {
     const fetchPartners = async () => {
       try {
-        const response = await axios.get('https://644060ba792fe886a88de1b9.mockapi.io/v1/test/partners');
+        const response = await axios.get(
+          'https://644060ba792fe886a88de1b9.mockapi.io/v1/test/partners'
+        );
         setPartners(response.data);
       } catch (error) {
         setError('Erro ao buscar parceiros');
@@ -37,7 +38,7 @@ const PartnersList = () => {
   const deletePartner = async (id) => {
     try {
       await axios.delete(`https://644060ba792fe886a88de1b9.mockapi.io/v1/test/partners/${id}`);
-      setPartners((prevPartners) => prevPartners.filter(partner => partner.id !== id));
+      setPartners((prevPartners) => prevPartners.filter((partner) => partner.id !== id));
     } catch (error) {
       console.error('Erro ao deletar parceiro:', error);
       setError('Erro ao deletar parceiro');
@@ -51,8 +52,13 @@ const PartnersList = () => {
 
   const updatePartner = async (updatedPartner) => {
     try {
-      const response = await axios.put(`https://644060ba792fe886a88de1b9.mockapi.io/v1/test/partners/${updatedPartner.id}`, updatedPartner);
-      setPartners((prevPartners) => prevPartners.map(partner => (partner.id === updatedPartner.id ? response.data : partner)));
+      const response = await axios.put(
+        `https://644060ba792fe886a88de1b9.mockapi.io/v1/test/partners/${updatedPartner.id}`,
+        updatedPartner
+      );
+      setPartners((prevPartners) =>
+        prevPartners.map((partner) => (partner.id === updatedPartner.id ? response.data : partner))
+      );
       setIsModalOpen(false);
       setCurrentPartner(null);
     } catch (error) {
@@ -62,28 +68,24 @@ const PartnersList = () => {
   };
 
   // Cálculo dos parceiros a serem exibidos na página atual
-  const indexOfLastPartner = currentPage * itemsPerPage; // Último índice do parceiro
-  const indexOfFirstPartner = indexOfLastPartner - itemsPerPage; // Primeiro índice do parceiro
-  const currentPartners = partners.slice(indexOfFirstPartner, indexOfLastPartner); // Seleciona os parceiros para a página atual
+  const indexOfLastPartner = currentPage * itemsPerPage;
+  const indexOfFirstPartner = indexOfLastPartner - itemsPerPage;
+  const currentPartners = partners.slice(indexOfFirstPartner, indexOfLastPartner);
 
-  // Total de páginas
   const totalPages = Math.ceil(partners.length / itemsPerPage);
 
-  if (loading) {
-    return <div>Carregando...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
+  if (loading) return <div>Carregando...</div>;
+  if (error) return <div>{error}</div>;
 
   return (
     <div>
       <h1>Lista de Parceiros</h1>
-      <button onClick={() => {
-        setCurrentPartner(null);
-        setIsModalOpen(true);
-      }}>
+      <button
+        onClick={() => {
+          setCurrentPartner(null);
+          setIsModalOpen(true);
+        }}
+      >
         Adicionar Parceiro
       </button>
       <div style={{ width: '100%', padding: '20px' }}>
@@ -101,8 +103,18 @@ const PartnersList = () => {
                 <td>{partner.id}</td>
                 <td>{partner.name}</td>
                 <td>
-                  <button onClick={() => editPartner(partner)} style={{ backgroundColor: '#535bf2'}}>Editar</button>
-                  <button onClick={() => deletePartner(partner.id)} style={{ backgroundColor: 'red', marginLeft: '10px' }}>Deletar</button>
+                  <button
+                    onClick={() => editPartner(partner)}
+                    style={{ backgroundColor: '#535bf2' }}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    onClick={() => deletePartner(partner.id)}
+                    style={{ backgroundColor: 'red', marginLeft: '10px' }}
+                  >
+                    Deletar
+                  </button>
                 </td>
               </tr>
             ))}
@@ -113,27 +125,30 @@ const PartnersList = () => {
       {/* Controles de Paginação */}
       <div style={{ marginTop: '20px' }}>
         {Array.from({ length: totalPages }, (_, index) => (
-          <button 
-            key={index + 1} 
-            onClick={() => setCurrentPage(index + 1)} 
-            style={{ margin: '0 5px', backgroundColor: currentPage === index + 1 ? '#ddd' : '#fff' }}
+          <button
+            key={index + 1}
+            onClick={() => onPageChange(index + 1)} // Chama o callback ao mudar de página
+            style={{
+              margin: '0 5px',
+              backgroundColor: currentPage === index + 1 ? '#ddd' : '#fff',
+            }}
           >
             {index + 1}
           </button>
         ))}
       </div>
 
-      <AddPartnerModal 
-        isOpen={isModalOpen && currentPartner === null} 
-        onRequestClose={() => setIsModalOpen(false)} 
-        onAddPartner={addPartner} 
+      <AddPartnerModal
+        isOpen={isModalOpen && currentPartner === null}
+        onRequestClose={() => setIsModalOpen(false)}
+        onAddPartner={addPartner}
       />
 
-      <PartnerModal 
-        isOpen={isModalOpen && currentPartner !== null} 
-        onRequestClose={() => setIsModalOpen(false)} 
-        onUpdatePartner={updatePartner} 
-        partner={currentPartner} 
+      <PartnerModal
+        isOpen={isModalOpen && currentPartner !== null}
+        onRequestClose={() => setIsModalOpen(false)}
+        onUpdatePartner={updatePartner}
+        partner={currentPartner}
       />
     </div>
   );
