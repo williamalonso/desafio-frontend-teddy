321dae20cf1a4c709ce70771435991bd4733f6c2
William <williamalonso@outlook.com>
2024-10-31 00:49:04 -0300

    :zap: vite conf

--- host/vite.config.js
+++ host/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from "@originjs/vite-plugin-federation";
 
+const remoteAppUrl = process.env.VITE_REMOTE_APP_URL || "http://localhost:5001/assets/remoteEntry.js";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -9,7 +11,7 @@ export default defineConfig({
     federation({
       name: "host_app",
       remotes: {
-        remoteApp: import.meta.env.VITE_REMOTE_APP_URL,
+        remoteApp: remoteAppUrl,
       },
       shared: ["react", "react-dom"],
     })
